dc9265ca63375c98a20ca8d393fa7c2b50aab111
curso-front <eromerog@live.com>
2018-03-14 21:22:51 +0100

    JavaScript + instalación node.js

---
+++ 12_js/02 function/js/app.js
@@ -0,0 +1,6 @@
+//Nombre en JS utilizan camelClase
+
+function saludar(){
+    console.log('Hola amigo')
+    }
+    
---
+++ 12_js/02 function/js/calculos.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//a y b son argumentos
+function sumar(a, b) {
+    var c = a + b
+    console.log('El resultado es', c)
+    return c
+}//fin de la función sumar
+function restar(a, b) {
+    var c = a - b
+    console.log('El resultado es', c)
+    return c
+}
+
+//paso 2 Parámetros
+sumar(123, 34);
+
+sumar(23, 45);
+
+sumar(2, 5);
+sumar()
+
+restar(10, 5)

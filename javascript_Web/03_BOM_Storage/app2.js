b754c79eff6647e940b1caa1598879b88161d96c
curso-front <eromerog@live.com>
2018-04-05 21:20:21 +0200

    storage enlace

--- javascript_Web/03_BOM_Storage/app2.js
+++ javascript_Web/03_BOM_Storage/app2.js
@@ -1,5 +1,5 @@
-(function(){
-    function app(){
+(function () {
+    function app() {
         let eNombre = document.querySelector("#inpNombre")
         let eApellido = document.querySelector("#inpApellido")
         let eGuardar = document.querySelector("#btnGuardar")
@@ -9,31 +9,28 @@
         eGuardar.addEventListener('click', guardar)
         eBorrar.addEventListener('click', borrar)
 
-        function guardar(){
+        function guardar() {
             let persona = {
-            nombre:eNombre.value,
-            apellido:eApellido.value
+                nombre: eNombre.value,
+                apellido: eApellido.value
+            }
+            localStorage.setItem('persona', JSON.stringify(persona))
+            saludar()
         }
-        localStorage.setItem('persona',JSON.stringify(persona))
-        saludar()
-    }
-        function borrar(){
+        function borrar() {
             localStorage.removeItem("persona")
             eSaludo.innerHTML = ''
             eNombre.value = ''
             eApellido.value = ''
         }
-        function saludar(){
-            let persona=localStorage.getItem('persona')
+        function saludar() {
+            let persona = localStorage.getItem('persona')
             persona = JSON.parse(persona)
-            if(persona.nombre || persona.apellido){
-                eSaludo.innerHTML = `Hola ${persona.nombre} ${persona.apellido}`
+            if (persona.nombre || persona.apellido) {
+                eSaludo.innerHTML = `Hola ${persona.nombre}${persona.apellido}`
             }
         }
-            
         saludar()
     }
-
-    window.addEventListener('load',app)
-
+    window.addEventListener('load', app)
 })()

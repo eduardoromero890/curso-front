79fee26cadb761b29217a2ed4dd0773c142f04c7
curso-front <eromerog@live.com>
2018-04-06 21:02:47 +0200

    DOM

---
+++ javascript_Web/04_DOM/reyes.js
@@ -0,0 +1,34 @@
+/* function r(y){
+    return document.getElementById(y);
+}
+let fichaNombres=["Isabel","Fernando","Carlos","Felipe","Juana","Luis","Alfonso","Juan Carlos","Amadeo","Jose"]
+let ficha = []
+
+function recoger(){
+    /* localStorage("ficha")=JSON.stringify(ficha) */
+    ficha.push(document.getElementById('nombre'.value)
+
+for(let i=0; i<fichaNombres.length;i++){
+    if(ficha == fichaNombres[i]){
+
+    }
+}
+document.getElementById('nombre').value;
+}
+function enseñar(){
+    document.getElementById()
+} */
+(function(){
+    function inicio()
+})
+function nuevoName(){
+    let ListaReyes = new Array('Isabel','Fernando','Carlos','Felipe','Juana','Luis','Alfonso','Juan Carlos','Amadeo','Jose')
+
+let mensaje = document.getElementById("respuesta");
+mensaje.innerHTML="";
+let cad = document.getElementById('inpRey').value
+let ListaReyesMay = ListaReyes.map(item=>item.toUppercase())
+let pos = ListaReyesMay.indexof(cad.toUppercase())
+if (pos >= 0)
+
+}

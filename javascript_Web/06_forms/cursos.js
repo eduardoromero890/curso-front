7d43eb30ff491e4b940f92645000e393996be3c3
curso-front <eromerog@live.com>
2018-04-12 19:53:41 +0200

    modificaciojnes forms

--- javascript_Web/06_forms/cursos.js
+++ javascript_Web/06_forms/cursos.js
@@ -1,12 +1,18 @@
 export const CURSOS = [
-    {título: 'Desarrollo Front',
-    asignaturas:[
-'HTML5', 'CSS5', 'JavaScript'
-    ]},
-    {titulo: 'Diseño Web',
-    asignaturas:[
-        'HTML5', 'CSS5', 'Ilustración',
-    ]},
-    {},
-    {}
+    {titulo: 'Desarrollo Front', 
+     asignaturas : [
+         'HTML5', 'CSS5', 'JavaScript'
+     ] },
+    {titulo: 'Diseño Web', 
+     asignaturas : [
+        'HTML5', 'CSS5', 'Ilustración', 'Accesibilidas'
+     ] },
+     {titulo: 'Servidores', 
+     asignaturas : [
+         'Node', '.Net', 'PHP', 'Java'
+     ] },
+     {titulo: 'Bases de Datos', 
+     asignaturas : [
+         'Diselo BBDD', 'SQL Server', 'MongoDB'
+     ] },
 ]
